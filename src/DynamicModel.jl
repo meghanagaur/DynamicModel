a63529ee0dd47569c000a98b3b337a3539941b1d
Meghana Gaur <gaur.meghana@gmail.com>
2022-07-07 18:18:14 -0500

    Update exports

--- src/DynamicModel.jl
+++ src/DynamicModel.jl
@@ -1,6 +1,7 @@
 module DynamicModel
 
-# Solve the dynamic EGSS model (first pass)
+#= Solve the dynamic EGSS model (first pass).
+Focus on the cae without savings. =#
 
 export model, solveModel, simulateProd
 
@@ -13,18 +14,17 @@ include("dep/rouwenhorst.jl")
 Simulate N {z_t} paths, given transition probs and 
 productivity grid, and return probability of each path.
 """
-function simulateProd(P_z, zgrid, T; N = 10000, seed = 111)
-
+function simulateProd(P_z, zgrid, T; N = 10000, seed = 111, z0 = median(1:length(zgrid)))
     Random.seed!(seed)
     sim      = rand(T, N)  # draw uniform numbers in (0,1)
     zt       = zeros(Int64, T, N)
-    zt[1,:] .= floor(Int64, median(1:length(zgrid)))
+    zt[1,:] .= floor(Int64, z0)
     CDF      = cumsum(P_z, dims = 2)
     probs    = ones(N)
 
     @inbounds for i = 1:N
         @inbounds for t = 2:T
-            zt[t, i]  = findfirst(x-> x >= sim[t,i], CDF[zt[t-1,i],:] ) 
+            zt[t, i]  = findfirst(x-> x >=  sim[t,i], CDF[zt[t-1,i],:]) 
             probs[i]  =  P_z[zt[t-1], zt[t]]*probs[i]
         end
     end
@@ -43,14 +43,15 @@ T   = final period (including t = 0)
 ω   = worker's present value from unemployment at time t
 b   = unemployment benefit (flow)
 σ_η = st dev of η distribution
-z0  = initial productivity
+μ_z = unconditional mean of log productivity
+z0  = initial log productivity
 ρ   = persistence of AR1 logz process
 σ_ϵ = variance of error in logz process
 ε   = Frisch elasticity, disutility of effort
 ψ   = pass-through parameters
 """
-function model(; T = 15, β = 0.99, s = 0.2, κ = 0.213, ι = 1.25, ε = 0.5,
-    σ_η = 0.1, ρ = 0.99, σ_ϵ = 0.1, b = 0.68, z0 = 1.0, N_z = 17, savings = false)
+function model(; T = 20, β = 0.99, s = 0.2, κ = 0.213, ι = 1.27, ε = 0.5, σ_η = 0.1, 
+    ρ = 0.99, σ_ϵ = 0.01, b = 0.68, z0 = 0.0, μ_z = z0, N_z = 21, savings = false)
 
     q(θ)    = 1/(1 + θ^ι)^(1/ι)                     # vacancy-filling rate
     v(c)    = log(c)                                # utility from consumption                
@@ -58,7 +59,7 @@ function model(; T = 15, β = 0.99, s = 0.2, κ = 0.213, ι = 1.25, ε = 0.5,
     u(c, a) = u(c) - h(a)                           # utility function
     hp(a)   = a^(1/ε)                               # h'(a)
 
-    logz, P_z = rouwenhorst(log(z0), ρ, σ_ϵ, N_z)   # discretized logz grid & transition probabilties
+    logz, P_z = rouwenhorst(μ_z, ρ, σ_ϵ, N_z)  # discretized logz grid & transition probabilties
     zgrid     = exp.(logz)                          # actual productivity grid
 
     # pass-through parameters. Note: there is a 0th period
@@ -71,7 +72,7 @@ function model(; T = 15, β = 0.99, s = 0.2, κ = 0.213, ι = 1.25, ε = 0.5,
     ω(t) = log(b)*(1 - β^(T-t+1))/(1-β)
     
     return (T = T, β = β, s = s, κ = κ, ι = ι, ε = ε, σ_η = σ_η, ρ = ρ,
-    σ_ϵ = σ_ϵ, ω = ω, z0 = z0, N_z = N_z, q = q, v = v, ψ = ψ,
+    σ_ϵ = σ_ϵ, ω = ω, μ_z = μ_z, N_z = N_z, q = q, v = v, ψ = ψ, z0 = z0,
     h = h, u = u, hp = hp, zgrid = zgrid, P_z = P_z, savings = savings)
 end
 
@@ -82,35 +83,35 @@ function solveModel(m; max_iter1 = 25, max_iter2 = 500, tol1 = 10^-5, tol2 =  10
     
     @unpack T, β, s, κ, ι, ε, σ_η, ω, N_z, q, u, h, hp, zgrid, P_z, ψ, savings = m   
     
-    # set tolerance parameters for outer loop
-    err1   = 10
-    err2   = 10
-    iter1  = 1
-    iter2  = 1
+    # set tolerance parameters for inner and outer loops
+    err1  = 10
+    err2  = 10
+    iter1 = 1
+    iter2 = 1
 
-    θ_lb = 0.0             # lower search bound
-    θ_ub = 1.0             # upper search bound
-    θ_0  = (θ_lb + θ_ub)/2 # initial guess for θ
-    Y_0  = 1.0             # initalize Y_0 for export
-    IR   = 1.0             # initalize IR for export
+    # Initialize default values and search parameters
+    θ_lb  = 0.0             # lower search bound
+    θ_ub  = 2.0             # upper search bound
+    θ_0   = (θ_lb + θ_ub)/2 # initial guess for θ
+    α     = 0.25            # dampening parameter
+    Y_0   = 0               # initalize Y_0 for export
+    IR    = 0               # initalize IR for export
+    w0    = 0               # initialize expected wage
 
-    #  simulate productivity draws
+    #  simulate productivity paths
     ZZ, probs   = simulateProd(P_z, zgrid, T+1)
-    #AZ          = similar(ZZ, size(ZZ))
     YY          = similar(ZZ, size(ZZ,2))
+    AZ          = similar(ZZ, size(ZZ))
 
-    # reduce computation time by looking at unique draws
+    # reduce computation of expectations by computing values only for unique sequences
     zz    = unique(ZZ, dims=2)
     az    = similar(zz')
     yy    = similar(zz')
+    flag  = zeros(Int64, size(zz'))
     idx   =  Dict{Int64, Vector{Int64}}()
     @inbounds for n = 1:size(zz,2)
         push!(idx, n => findall(isequal(T+1), vec(sum(ZZ .== zz[:,n], dims=1))) )
     end   
-    flag  = zeros(Int64, size(zz'))
-
-    # Dampening parameter
-    α     = 0.25
 
     # look for a fixed point in θ
     @inbounds while err1 > tol1 && iter1 < max_iter1
@@ -135,13 +136,13 @@ function solveModel(m; max_iter1 = 25, max_iter2 = 500, tol1 = 10^-5, tol2 =  10
                     if ε == 1 # can solve analytically
                         aa = (z/w0 + sqrt((z/w0)^2))/2(1 + ψ_t*σ_η^2)
                     else # exclude the choice of zero effort
-                        aa = find_zeros(x -> x - max(z*x/w0 -  (ψ_t/ε)*(hp(x)*σ_η)^2, 0)^(ε/(1+ε)) + Inf*(x==0) , eps(), Y_0) 
+                        aa = find_zeros(x -> x - max(z*x/w0 -  (ψ_t/ε)*(hp(x)*σ_η)^2, 0)^(ε/(1+ε)) + Inf*(x==0), 0, 10) 
                     end
 
                     # create a flag matrix -- decide how to handle any violations
                     idx1             = findall(isequal(z), zz[t+1, :])
                     az[idx1,t+1]    .= isempty(aa) ? 0 : aa[1]
-                    flag[idx1,t+1]  .= ((z*az[n, t+1]/w0 + (ψ_t/ε)*(hp(az[n,t+1])*σ_η)^2) < 0) + isempty(aa) #+ (w0 < 0)
+                    flag[idx1,t+1]  .= ((z*az[n, t+1]/w0 + (ψ_t/ε)*(hp(az[n,t+1])*σ_η)^2) < 0) + isempty(aa) + (w0 < 0)
                     yy[idx1,t+1]    .= ((β*(1-s))^(t))*az[n,t+1]*z
                 end  
             end
@@ -150,7 +151,6 @@ function solveModel(m; max_iter1 = 25, max_iter2 = 500, tol1 = 10^-5, tol2 =  10
             y = sum(yy,dims=2)
             @inbounds for n = 1:size(zz,2)
                 YY[idx[n]]    .= y[n]
-                #AZ[:,idx]  .= az[n,:]
             end   
 
             # Numerical approximation of E_0[Y]
@@ -163,8 +163,7 @@ function solveModel(m; max_iter1 = 25, max_iter2 = 500, tol1 = 10^-5, tol2 =  10
                 Y_lb  = copy(Y_0)
             end
 
-            #Y_0  = (Y_lb + Y_ub)/2  # converges slowly
-            Y_0  = α*Y_0 + (1-α)*Y_1 # converges faster
+            Y_0  = α*Y_0 + (1-α)*Y_1
             #println(Y_0)
             iter2 += 1
         end
@@ -199,15 +198,20 @@ function solveModel(m; max_iter1 = 25, max_iter2 = 500, tol1 = 10^-5, tol2 =  10
             θ_ub  = copy(θ_0)
         end
 
-        if noisy == true
+        if noisy 
             println(θ_0)
         end
         θ_0 = (θ_lb + θ_ub)/2
         iter1 += 1
     end
 
-    return (θ = θ_0, flags = flag, Y = Y_0, V = IR, ω0 = ω(0), err1 = err1, 
-    err2 = err2, iter1 = iter1, iter2 = iter2) 
+    @inbounds for n = 1:size(zz,2)
+        AZ[:, idx[n]]   .= az[n,:]
+    end
+
+    return (θ = θ_0, Y = Y_0, V = IR, ω0 = ω(0), w0 = w0, mod = m,
+    idx = idx, zz = zz, ZZ = ZZ, az = az, AZ = AZ, flags = flag, 
+    err1 = err1, err2 = err2, iter1 = iter1, iter2 = iter2) 
 end
 
 end # module
