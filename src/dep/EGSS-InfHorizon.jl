d07a3674128cbe5b396affc29389115276ea0657
Meghana Gaur <gaur.meghana@gmail.com>
2022-08-31 19:45:54 -0400

    minor fixes

--- src/dep/EGSS-InfHorizon.jl
+++ src/dep/EGSS-InfHorizon.jl
@@ -26,7 +26,7 @@ procyclical == (procyclical unemployment benefit)
 """ 
 #ρ =  0.97 (quarterly - linear time trend)   # ι = 1.25 (PNZ = monthly)
 #σ_ϵ = 0.008 (quarterly - linear time trend) # κ =  0.213 (Shimer)
-function model(; β = 0.99, s = 0.1, κ = 0.474, ι = 1.67, ε = 0.5, σ_η = 0.05, z_ss = 1.0,
+function model(; β = 0.99, s = 0.1, κ = 0.474, ι = 3.05, ε = 0.5, σ_η = 0.05, z_ss = 1.0,
     ρ =  0.87, σ_ϵ = 0.008, χ = 0.1, γ = 0.66, z0 = z_ss, μ_z = log(z_ss), N_z = 11, procyclical = true)
 
     # Basic parameterization
