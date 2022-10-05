92665c4e2440a17705f56346568a86831f65f1fb
Meghana Gaur <gaur.meghana@gmail.com>
2022-10-05 13:24:55 -0400

    update mf and add hbar

--- src/dep/EGSS-InfHorizon.jl
+++ src/dep/EGSS-InfHorizon.jl
@@ -25,47 +25,54 @@ z_1  = initial prod. (= z_ss by default)
 
 procyclical == (procyclical unemployment benefit)
 """ 
-#ρ =  0.97 (quarterly - linear time trend)   # ι = 1.25 (PNZ = monthly)
-#σ_ϵ = 0.008 (quarterly - linear time trend) # κ =  0.213 (Shimer), s = 0.1 (Shimer)
-function model(; β = 0.99, s = 0.1, κ = 0.474, μ = 1.355, α = 0.72, ε = 1.0, σ_η = 0.05, z_ss = 1.0,
+#=
+Quarterly:
+function model(; β = 0.99, s = 0.088, κ = 0.474, ι = 1.67, ε = 0.5, σ_η = 0.05, z_ss = 1.0,
     ρ =  0.87, σ_ϵ = 0.008, χ = 0.1, γ = 0.66, z_1 = z_ss, μ_z = log(z_ss), N_z = 11, procyclical = true)
+Quarterly->monthly
+ρ = 0.87^(1/3)
+sqrt(0.017^2 / mapreduce(j-> ρ^(2j), +, [0:2;]))
+=#
+function model(; β = 0.99^(1/3), s = 0.035, κ = 0.45, ε = 0.5, σ_η = 0.01, z_ss = 1.0, μ = 0.42,
+    α = 0.72, hbar = 1, ρ =  0.87^(1/3), σ_ϵ = 0.01, χ = 0.0, γ = 0.66, z_1 = z_ss, N_z = 17)
 
     # Basic parameterization
-    q(θ)    = μ*θ^(-α)                              # vacancy-filling rate
-    f(θ)    = μ*θ^(1-α)                             # job-finding rate
-    u(c)    = log(max(c, eps()))                    # utility from consumption                
-    h(a)    = (a^(1 + 1/ε))/(1 + 1/ε)               # disutility from effort  
-    u(c, a) = u(c) - h(a)                           # utility function
-    hp(a)   = a^(1/ε)                               # h'(a)
-    r       = 1/β -1                                # interest rate        
+    q(θ)    = μ*θ^(-α)                      # vacancy-filling rate
+    f(θ)    = μ*θ^(1-α)                     # job-filling rate
+    u(c)    = log(max(c, eps()))            # utility from consumption                
+    h(a)    = hbar*(a^(1 + 1/ε))/(1 + 1/ε)  # disutility from effort  
+    u(c, a) = u(c) - h(a)                   # utility function
+    hp(a)   = hbar*a^(1/ε)                  # h'(a)
 
     # Define productivity grid
     if (iseven(N_z)) error("N_z must be odd") end 
-    logz, P_z = rouwenhorst(μ_z, ρ, σ_ϵ, N_z)        # discretized logz grid & transition probabilties
-    zgrid     = exp.(logz)                           # actual productivity grid
-    z_1_idx   = findfirst(isapprox(z_1), zgrid)      # index of z0 on zgrid
+    μ_z       = log(z_ss) - ((1-ρ)*σ_ϵ^2)/(2*(1-ρ^2))
+    logz, P_z = rouwenhorst(μ_z, ρ, σ_ϵ, N_z)                    # discretized logz grid & transition probabilties
+    zgrid     = exp.(logz)                                       # actual productivity grid
+    z_1_idx   = findfirst(isapprox(z_1, atol = 0.0001), zgrid)   # index of z0 on zgrid
 
     # Pass-through parameter
     ψ    = 1 - β*(1-s)
 
     # Unemployment benefit given aggregate state: (z) 
-    if procyclical == true
+    if isapprox(χ, 0) 
+        procyclical = false
+    else
+        procyclical = true
         ξ(z) = (γ)*(z/z_ss)^χ 
-    elseif procyclical == false
-        ξ    = γ
     end
 
     # PV of unemp = PV of utility from consuming unemployment benefit forever
     if procyclical == false
-        ω = log(ξ)/(1-β) # scalar
+        ω = log(γ)/(1-β) # scalar
     elseif procyclical == true
         #println("Solving for value of unemployment...")
         ω = unemploymentValue(β, ξ, u, zgrid, P_z).v0 # N_z x 1
     end
     
-    return (β = β, r = r, s = s, κ = κ, μ = μ, α = α, ε = ε, σ_η = σ_η, ρ = ρ, σ_ϵ = σ_ϵ, z_ss = z_ss,
-    ω = ω, μ_z = μ_z, N_z = N_z, q = q, f = f, ψ = ψ, z_1 = z_1, h = h, u = u, hp = hp, 
-    z_1_idx = z_1_idx, zgrid = zgrid, P_z = P_z, ξ = ξ, χ = χ, γ = γ, procyclical = procyclical)
+    return (β = β, s = s, κ = κ, ε = ε, σ_η = σ_η, ρ = ρ, σ_ϵ = σ_ϵ, z_ss = z_ss, μ = μ,
+     α = α, ω = ω, N_z = N_z, q = q, f = f, ψ = ψ, z_1 = z_1, h = h, u = u, hp = hp, 
+    z_1_idx = z_1_idx, zgrid = zgrid, P_z = P_z, χ = χ, γ = γ, procyclical = procyclical)
 end
 
 """
@@ -98,7 +105,7 @@ Solve the infinite horizon EGSS model using a bisection search on θ.
 function solveModel(modd; max_iter1 = 50, max_iter2 = 1000, max_iter3 = 1000,
     tol1 = 10^-6, tol2 = 10^-8, tol3 =  10^-8, noisy = true, q_lb_0 =  0.0, q_ub_0 = 1.0)
 
-    @unpack β, r, s, κ, μ, α, ε, σ_η, ω, N_z, q, u, h, hp, zgrid, P_z, ψ, procyclical, N_z, z_1_idx = modd  
+    @unpack β, s, κ, μ, α, ε, σ_η, ω, N_z, q, u, h, hp, zgrid, P_z, ψ, procyclical, N_z, z_1_idx = modd  
 
     # set tolerance parameters for outermost loop
     err1  = 10
@@ -114,6 +121,7 @@ function solveModel(modd; max_iter1 = 50, max_iter2 = 1000, max_iter3 = 1000,
     q_lb   = q_lb_0          # lower search bound for θ
     q_ub   = q_ub_0          # upper search bound for θ
     q_0    = (q_lb + q_ub)/2 # initial guess for θ
+    l      = 0               # dampening parameter
     Y_0    = 0               # initalize Y for export
     U      = 0               # initalize worker's EU from contract for export
     w_0    = 0               # initialize initial wage constant for export
@@ -146,21 +154,23 @@ function solveModel(modd; max_iter1 = 50, max_iter2 = 1000, max_iter3 = 1000,
             if (err2 > tol2) 
                 iter2 += 1
                 if (iter2 < max_iter2) 
-                    Y_0    = copy(Y_1) 
+                    Y_0    = l*Y_0 + (1-l)*Y_1 
                 end
             end
+            #println(Y_0[z_1_idx])
         end
 
+        # Solve recursively for the PV utility from the contract
         err3  = 10
         iter3 = 1  
-
-        # Solve recursively for the PV utility from the contract
-        W_0   = procyclical ?  copy(ω) : ω*ones(N_z) # initial guess
-        flow  = -(1/2ψ)*(ψ*hp.(az)*σ_η).^2 - h.(az) + β*s*(P_z*ω)
+        ω_vec = procyclical ?  copy(ω) : ω*ones(N_z)
+        W_0   = copy(ω_vec) # initial guess
+        flow  = -(1/(2ψ))*(ψ*hp.(az)*σ_η).^2 - h.(az) + β*s*(P_z*ω_vec)
         @inbounds while err3 > tol3 && iter3 <= max_iter3
             W_1  = flow + β*(1-s)*(P_z*W_0)
             err3 = maximum(abs.(W_1 - W_0))
-            W_0  = copy(W_1)
+            W_0  = l*W_0 + (1-l)*W_1
+            #println(W_0[z_1_idx])
             iter3 +=1
         end
         
@@ -181,22 +191,20 @@ function solveModel(modd; max_iter1 = 50, max_iter2 = 1000, max_iter3 = 1000,
             if (iter1 < max_iter1) 
                 q_0    = (q_lb + q_ub)/2
                 # exit loop if q is stuck near the bounds 
-                if min(abs(q_0 - q_ub_0), abs(q_0 - q_lb_0)) < 10^-5
+                if min(abs(q_0 - q_ub_0), abs(q_0 - q_lb_0)) <= 10^-6
                     # check if the  IR constraint is satisfied
-                    #= if U > ω_0
+                    if U > ω_0
                         break
                     else  
                         iter1 = max_iter1 + 1              
-                    end =#
-                    iter1 = max_iter1 + 1              
+                    end 
+                    #iter1 = max_iter1 + 1              
                 end
             end
         end
     end
 
-    θ = (μ/q_0)^(1/α) #(q_0^(-ι) - 1)^(1/ι)
-
-    return (θ = θ, Y = Y_0[z_1_idx], U = U, ω_0 = ω_0, w_0 = w_0, mod = modd, 
+    return (θ = (μ/q_0)^(1/α), Y = Y_0[z_1_idx], U = U, ω_0 = ω_0, w_0 = w_0, mod = modd, 
     az = az, yz = yz, err1 = err1, err2 = err2, err3 = err3, iter1 = iter1, iter2 = iter2, iter3 = iter3, wage_flag = (w_0 <= 0),
     effort_flag = maximum(a_flag), exit_flag1 = (iter1 > max_iter1), exit_flag2 = (iter2 > max_iter2), exit_flag3 = (iter3 > max_iter3))
 end
