8e9aa22bc25ae99248d9ead4e9e7e1ab744d63c6
Meghana Gaur <gaur.meghana@gmail.com>
2022-10-19 14:44:16 -0400

    change matching function, lower unemp benefit

--- src/dep/EGSS-InfHorizon.jl
+++ src/dep/EGSS-InfHorizon.jl
@@ -20,6 +20,7 @@ z_1  = initial prod. (= log(z_ss) by default)
 ρ    = persistence of log prod. process
 σ_ϵ  = variance of noise in log prod. process
 ε    = Frisch elasticity: disutility of effort
+hbar = disutility of effort - level
 ψ    = pass-through parameter
 
 procyclical == (procyclical unemployment benefit)
@@ -28,17 +29,17 @@ procyclical == (procyclical unemployment benefit)
 Quarterly:
 function model(; β = 0.99, s = 0.088, κ = 0.474, ι = 1.67, ε = 0.5, σ_η = 0.05, z_ss = 1.0,
     ρ =  0.87, σ_ϵ = 0.008, χ = 0.1, γ = 0.66, z_1 = z_ss, μ_z = log(z_ss), N_z = 11, procyclical = true)
-Quarterly->monthly
+Quarterly -> monthly
 ρ = 0.87^(1/3)
 sqrt(0.017^2 / mapreduce(j-> ρ^(2j), +, [0:2;])) = 0.01
 =#
-function model(; β = 0.99^(1/3), s = 0.03, κ = 0.47, ε = 0.5, σ_η = 0.5, z_ss = 1.0, μ = 0.42,
-    α = 0.72, hbar = 1.0, ρ =  0.95^(1/3), σ_ϵ = 0.0065, χ = 0.0, γ = 0.7, z_1 = z_ss, N_z = 11)
+function model(; β = 0.99^(1/3), s = 0.035, κ = 0.45, ε = 0.5, σ_η = 0.5, z_ss = 1.0, ι = 0.7,
+    hbar = 1.0, ρ =  0.95^(1/3), σ_ϵ = 0.0065, χ = 0.0, γ = 0.6, z_1 = z_ss, N_z = 11)
 
     # Basic parameterization
-    q(θ)    = μ*θ^(-α)                      # job-filling rate
-    f(θ)    = μ*θ^(1-α)                     # job-finding rate
-    u(c)    = log(max(c, 10^-6))            # utility from consumption                
+    q(θ)    = (1 + θ^ι)^(-1/ι)              # job-filling rate
+    f(θ)    = (1 + θ^-ι)^(-1/ι)             # job-finding rate
+    u(c)    = log(max(c, 10^-8))            # utility from consumption                
     h(a)    = hbar*(a^(1 + 1/ε))/(1 + 1/ε)  # disutility from effort  
     hp(a)   = hbar*a^(1/ε)                  # h'(a)
     u(c, a) = u(c) - h(a)                   # overall utility function
@@ -69,8 +70,8 @@ function model(; β = 0.99^(1/3), s = 0.03, κ = 0.47, ε = 0.5, σ_η = 0.5, z_
         ω = unemploymentValue(β, ξ, u, zgrid, P_z).v0 # N_z x 1
     end
     
-    return (β = β, s = s, κ = κ, ε = ε, σ_η = σ_η, ρ = ρ, σ_ϵ = σ_ϵ, z_ss = z_ss, μ = μ,
-     α = α, hbar = hbar, ω = ω, N_z = N_z, q = q, f = f, ψ = ψ, z_1 = z_1, h = h, u = u, hp = hp, 
+    return (β = β, s = s, κ = κ, ε = ε, σ_η = σ_η, ρ = ρ, σ_ϵ = σ_ϵ, z_ss = z_ss, μ_z = μ_z,
+    ι, hbar = hbar, ω = ω, N_z = N_z, q = q, f = f, ψ = ψ, z_1 = z_1, h = h, u = u, hp = hp, 
     z_1_idx = z_1_idx, zgrid = zgrid, P_z = P_z, χ = χ, γ = γ, procyclical = procyclical)
 end
 
@@ -102,9 +103,9 @@ end
 Solve the infinite horizon EGSS model using a bisection search on θ.
 """
 function solveModel(modd; max_iter1 = 50, max_iter2 = 1000, max_iter3 = 1000,
-    tol1 = 10^-6, tol2 = 10^-8, tol3 =  10^-8, noisy = true, q_lb_0 =  0.0, q_ub_0 = 20.0)
+    tol1 = 10^-6, tol2 = 10^-8, tol3 =  10^-8, noisy = true, q_lb_0 =  0.0, q_ub_0 = 1.0)
 
-    @unpack β, s, κ, μ, α, ε, σ_η, ω, N_z, q, u, h, hp, zgrid, P_z, ψ, procyclical, N_z, z_1_idx = modd  
+    @unpack β, s, κ, ι, ε, σ_η, ω, N_z, q, u, h, hp, zgrid, P_z, ψ, procyclical, N_z, z_1_idx = modd  
 
     # set tolerance parameters for outermost loop
     err1  = 10
@@ -114,13 +115,14 @@ function solveModel(modd; max_iter1 = 50, max_iter2 = 1000, max_iter3 = 1000,
     iter2 = 1
     err3  = 10
     iter3 = 1
+    IR_err = 10
 
     # Initialize default values and search parameters
     ω_0    = procyclical ? ω[z_1_idx] : ω # unemployment value at z_1
     q_lb   = q_lb_0          # lower search bound for q
     q_ub   = q_ub_0          # upper search bound for q
     q_0    = (q_lb + q_ub)/2 # initial guess for q
-    l      = 0               # dampening parameter
+    α      = 0               # dampening parameter
     Y_0    = 0               # initalize Y for export
     U      = 0               # initalize worker's EU from contract for export
     w_0    = 0               # initialize initial wage constant for export
@@ -153,7 +155,7 @@ function solveModel(modd; max_iter1 = 50, max_iter2 = 1000, max_iter3 = 1000,
             if (err2 > tol2) 
                 iter2 += 1
                 if (iter2 < max_iter2) 
-                    Y_0    = l*Y_0 + (1-l)*Y_1 
+                    Y_0    = α*Y_0 + (1 - α)*Y_1 
                 end
             end
             #println(Y_0[z_1_idx])
@@ -162,20 +164,25 @@ function solveModel(modd; max_iter1 = 50, max_iter2 = 1000, max_iter3 = 1000,
         # Solve recursively for the PV utility from the contract
         err3  = 10
         iter3 = 1  
+
         ω_vec = procyclical ?  copy(ω) : ω*ones(N_z)
         W_0   = copy(ω_vec) # initial guess
         flow  = -(1/(2ψ))*(ψ*hp.(az)*σ_η).^2 - h.(az) + β*s*(P_z*ω_vec)
+
         @inbounds while err3 > tol3 && iter3 <= max_iter3
             W_1  = flow + β*(1-s)*(P_z*W_0)
             err3 = maximum(abs.(W_1 - W_0))
-            W_0  = l*W_0 + (1-l)*W_1
+            if (err3 > tol3) 
+                iter3 += 1
+                if (iter3 < max_iter3) 
+                    W_0  = α*W_0 + (1 - α)*W_1
+                end
+            end
             #println(W_0[z_1_idx])
-            iter3 +=1
         end
         
         # Check the IR constraint (must bind)
         U      = (1/ψ)*log(max(eps(), w_0)) + W_0[z_1_idx] # nudge w_0 to avoid runtime error
-        err1   = abs(U - ω_0)
         
         # Upate θ accordingly: note U is decreasing in θ (=> increasing in q)
         if U < ω_0              # increase q (decrease θ)
@@ -184,27 +191,25 @@ function solveModel(modd; max_iter1 = 50, max_iter2 = 1000, max_iter3 = 1000,
             q_ub  = copy(q_0)
         end
 
-        # export the accurate iter & q value
-        if (err1 > tol1)
-            iter1 += 1
-            if (iter1 < max_iter1) 
-                q_0    = (q_lb + q_ub)/2
-                # exit loop if q is stuck at the bounds 
-                if min(abs(q_0 - q_ub_0), abs(q_0 - q_lb_0)) < 10^-4
-                    # check if the  IR constraint is satisfied
-                    if U >= ω_0
-                        break
-                    else  
-                        iter1 = max_iter1 + 1              
-                    end 
-                end
-            end
-        end
-    end
+        # Bisection
+        IR_err = U - ω_0                            # check whether IR constraint holds
+        q_1    = (q_lb + q_ub)/2                    # update q
+        err1   = min(abs(IR_err), abs(q_1 - q_0))   # compute convergence criterion
 
-    return (θ = (μ/q_0)^(1/α), Y = Y_0[z_1_idx], U = U, ω_0 = ω_0, w_0 = w_0, mod = modd, 
+        # export the accurate iter & q value
+        if err1 > tol1
+            q_0     = α*q_0 + (1 - α)*q_1
+            iter1  += 1
+        end
+
+    end
+    
+    # Record info on IR constraint
+    flag_IR = (IR_err < 0)*(abs(IR_err) > tol1)
+
+    return (θ = (q_0^(-ι) - 1)^(1/ι), Y = Y_0[z_1_idx], U = U, ω_0 = ω_0, w_0 = w_0, mod = modd, IR_err = IR_err, flag_IR = flag_IR,
     az = az, yz = yz, err1 = err1, err2 = err2, err3 = err3, iter1 = iter1, iter2 = iter2, iter3 = iter3, wage_flag = (w_0 <= 0),
-    effort_flag = maximum(a_flag), exit_flag1 = (iter1 > max_iter1), exit_flag2 = (iter2 > max_iter2), exit_flag3 = (iter3 > max_iter3))
+    effort_flag = maximum(a_flag), conv_flag1 = (iter1 > max_iter1), conv_flag2 = (iter2 > max_iter2), conv_flag3 = (iter3 > max_iter3))
 end
 
 """
