e38fd57e1611c6920591259b2bb65fd595c0a31e
Meghana Gaur <gaur.meghana@gmail.com>
2023-02-25 18:37:41 -0500

    minor changes

--- src/dep/EGSS-InfHorizon.jl
+++ src/dep/EGSS-InfHorizon.jl
@@ -13,9 +13,9 @@ s    = exogenous separation rate
 ι    = controls elasticity of matching function 
 κ    = vacancy-posting cost
 ω    = worker's PV from unemployment (infinite horizon)
-χ    = prop. of unemp benefit to z / actual unemp benefit
-γ    = intercept for unemp benefit w / procyclical benefit
-z_ss = mean productivity (this is just a definition)
+χ    = elasticity of unemp benefit to z 
+γ    = level of unemp benefit 
+zbar = mean productivity
 σ_η  = st dev of η distribution
 μ_z  = unconditional mean of log productivity 
 ρ    = persistence of log productivity
@@ -26,7 +26,7 @@ hbar = disutility of effort - level
 
 procyclical == (procyclical unemployment benefit)
 """ 
-function model(; β = 0.99^(1/3), s = 0.031, κ = 0.45, ε = 0.5, σ_η = 0.5, z_ss = 1.0, ι = 0.8,
+function model(; β = 0.99^(1/3), s = 0.031, κ = 0.45, ε = 0.5, σ_η = 0.5, zbar = 1.0, ι = 0.8,
     hbar = 1.0, ρ =  0.95^(1/3), σ_ϵ = 0.003, χ = 0.0, γ = 0.6, N_z = 11)
 
     # Basic parameterization
@@ -39,7 +39,7 @@ function model(; β = 0.99^(1/3), s = 0.031, κ = 0.45, ε = 0.5, σ_η = 0.5, z
 
     # Define productivity grid
     if (iseven(N_z)) error("N_z must be odd") end 
-    μ_z             = log(z_ss) - (σ_ϵ^2)/(2*(1-ρ^2))   # normalize, so that E[z_t] = 1
+    μ_z             = log(zbar) - (σ_ϵ^2)/(2*(1-ρ^2))   # normalize E[logz], so that E[z_t] = 1
     logz, P_z, p_z  = rouwenhorst(μ_z, ρ, σ_ϵ, N_z)     # log z grid, transition matrix, invariant distribution
     zgrid           = exp.(logz)                        # z grid in levels
     z_ss_idx        = findfirst(isapprox(μ_z, atol = 1e-6), logz )
@@ -62,7 +62,7 @@ function model(; β = 0.99^(1/3), s = 0.031, κ = 0.45, ε = 0.5, σ_η = 0.5, z
         ω = unemploymentValue(β, ξ, u, zgrid, P_z).v0 # N_z x 1
     end
     
-    return (β = β, s = s, κ = κ, ε = ε, σ_η = σ_η, ρ = ρ, σ_ϵ = σ_ϵ, z_ss = z_ss, μ_z = μ_z,
+    return (β = β, s = s, κ = κ, ε = ε, σ_η = σ_η, ρ = ρ, σ_ϵ = σ_ϵ, zbar = zbar, μ_z = μ_z,
     ι = ι, hbar = hbar, ω = ω, N_z = N_z, q = q, f = f, ψ = ψ, h = h, u = u, hp = hp, z_ss_idx = z_ss_idx,
     logz = logz, zgrid = zgrid, P_z = P_z, p_z = p_z, χ = χ, γ = γ, procyclical = procyclical)
 end
@@ -70,7 +70,7 @@ end
 """
 Solve for the optimal effort a(z | z_0), given Y(z_0), θ(z_0), and z.
 Note: a_min > 0 to allow for numerical error. 
-If check_min == true, then root-finding checks for multiple roots. However, this is slow.
+If check_min == true, then root-finding checks for multiple roots (slow).
 """
 function optA(z, modd, w_0; a_min = 10^-6, a_max = 100.0, check_mult = false)
    
@@ -116,16 +116,16 @@ end
 """
 Solve the infinite horizon EGSS model using a bisection search on θ.
 """
-function solveModel(modd; z_1 = nothing, max_iter1 = 50, max_iter2 = 1000, max_iter3 = 1000, a_min = 10^-6,
+function solveModel(modd; z_0 = nothing, max_iter1 = 50, max_iter2 = 1000, max_iter3 = 1000, a_min = 10^-6,
     tol1 = 10^-8, tol2 = 10^-8, tol3 =  10^-8, noisy = true, q_lb_0 =  0.0, q_ub_0 = 1.0, check_mult = false)
 
     @unpack β, s, κ, ι, ε, σ_η, ω, N_z, q, u, h, hp, zgrid, P_z, ψ, procyclical, N_z, z_ss_idx = modd  
     
-    # find index of z_1 on the productivity grid 
-    if isnothing(z_1)
-        z_1_idx = z_ss_idx
+    # find index of z_0 on the productivity grid 
+    if isnothing(z_0)
+        z_0_idx = z_ss_idx
     else
-        z_1_idx = findfirst(isapprox(z_1, atol = 1e-6), zgrid)  
+        z_0_idx = findfirst(isapprox(z_0, atol = 1e-6), zgrid)  
     end
 
     # set tolerance parameters for outermost loop
@@ -140,7 +140,7 @@ function solveModel(modd; z_1 = nothing, max_iter1 = 50, max_iter2 = 1000, max_i
     flag_IR = 0
 
     # Initialize default values and search parameters
-    ω_0    = procyclical ? ω[z_1_idx] : ω # unemployment value at z_0
+    ω_0    = procyclical ? ω[z_0_idx] : ω # unemployment value at z_0
     ω_vec  = procyclical ?  copy(ω) : ω*ones(N_z)
     q_lb   = q_lb_0          # lower search bound for q
     q_ub   = q_ub_0          # upper search bound for q
@@ -151,27 +151,29 @@ function solveModel(modd; z_1 = nothing, max_iter1 = 50, max_iter2 = 1000, max_i
     w_0    = 0               # initialize initial wage constant for export
 
     # Initialize series
-    az     = zeros(N_z)   # a(z|z_1)                         
-    yz     = zeros(N_z)   # y(z|z_1)                         
-    a_flag = zeros(N_z)   # flag for a(z|z_1)                         
+    az     = zeros(N_z)   # a(z|z_0)                         
+    yz     = zeros(N_z)   # y(z|z_0)                         
+    a_flag = zeros(N_z)   # flag for a(z|z_0)                         
 
     # Look for a fixed point in θ_0
     @inbounds while err1 > tol1 && iter1 <= max_iter1  
 
         if noisy 
-            println(q_0)
+            println("iter:\t"*string(iter1))
+            println("error:\t"*string(err1))
+            println("q_0:\t"*string(q_0))
         end
 
-        # Look for a fixed point in Y(z | z_1), ∀ z
+        # Look for a fixed point in Y(z | z_0), ∀ z
         err2   = 10
         iter2  = 1      
-        Y_0    = ones(N_z)*(50*κ/q_0)   # initial guess for Y(z | z_1)
+        Y_0    = ones(N_z)*(50*κ/q_0)   # initial guess for Y(z | z_0)
         
         @inbounds while err2 > tol2 && iter2 <= max_iter2   
            
-            w_0  = ψ*(Y_0[z_1_idx] - κ/q_0) # constant for wage difference equation
+            w_0  = ψ*(Y_0[z_0_idx] - κ/q_0) # constant for wage difference equation
            
-            # Solve for optimal effort a(z | z_1)
+            # Solve for optimal effort a(z | z_0)
             @inbounds for (iz,z) in enumerate(zgrid)
                 az[iz], yz[iz], a_flag[iz] = optA(z, modd, w_0; check_mult = check_mult, a_min = a_min)
             end
@@ -183,7 +185,7 @@ function solveModel(modd; z_1 = nothing, max_iter1 = 50, max_iter2 = 1000, max_i
                     Y_0    = α*Y_0 + (1 - α)*Y_1 
                 end
             end
-            #println(Y_0[z_1_idx])
+            #println(Y_0[z_0_idx])
         end
 
         # Solve recursively for the PV utility from the contract
@@ -201,13 +203,13 @@ function solveModel(modd; z_1 = nothing, max_iter1 = 50, max_iter2 = 1000, max_i
                     W_0  = α*W_0 + (1 - α)*W_1
                 end
             end
-            #println(W_0[z_1_idx])
+            #println(W_0[z_0_idx])
         end
         
         # Check the IR constraint (must bind)
-        U      = (1/ψ)*log(max(eps(), w_0)) + W_0[z_1_idx] # nudge w_0 to avoid runtime error
+        U      = (1/ψ)*log(max(eps(), w_0)) + W_0[z_0_idx] # nudge w_0 to avoid runtime error
         
-        # Upate θ accordingly: note U is decreasing in θ (=> increasing in q)
+        # Upate θ accordingly: U decreasing in θ (increasing in q)
         if U < ω_0              # increase q (decrease θ)
             q_lb  = copy(q_0)
         elseif U > ω_0          # decrease q (increase θ)
@@ -235,7 +237,7 @@ function solveModel(modd; z_1 = nothing, max_iter1 = 50, max_iter2 = 1000, max_i
 
     end
 
-    return (θ = (q_0^(-ι) - 1)^(1/ι), Y = Y_0[z_1_idx], U = U, ω = ω_0, w_0 = w_0, mod = modd, IR_err = IR_err*flag_IR, flag_IR = flag_IR,
+    return (θ = (q_0^(-ι) - 1)^(1/ι), Y = Y_0[z_0_idx], U = U, ω = ω_0, w_0 = w_0, mod = modd, IR_err = IR_err*flag_IR, flag_IR = flag_IR,
     az = az, yz = yz, err1 = err1, err2 = err2, err3 = err3, iter1 = iter1, iter2 = iter2, iter3 = iter3, wage_flag = (w_0 <= 0),
     effort_flag = maximum(a_flag), conv_flag1 = (iter1 > max_iter1), conv_flag2 = (iter2 > max_iter2), conv_flag3 = (iter3 > max_iter3))
 end
--- src/dep/simulate-prod-paths.jl
+++ src/dep/simulate-prod-paths.jl
@@ -3,16 +3,16 @@ Simulate N {z_t} paths, given transition prob. matrix P_z and
 productivity grid, and return probability of each path. 
 Simulation begins at z0_idx of zgrid.
 """
-function simulateProd(P_z, zgrid, T; N = 10000, z_1_idx = median(1:length(zgrid)), seed = 211, set_seed = true)
+function simulateProd(P_z, zgrid, T; N = 10000, z0_idx = median(1:length(zgrid)), seed = 211, set_seed = true)
    
     if set_seed == true
         Random.seed!(seed)
     end
     
-    sim      = rand(N, T)            # N X T - draw uniform numbers in (0,1)
-    zt       = zeros(Int64, N, T)    # N X T - index on zgrid
-    zt[:,1] .= floor(Int64, z_1_idx) # Set z0 with valid index
-    CDF      = cumsum(P_z, dims = 2) # CDF for transition probs. given initial state
+    sim      = rand(N, T)             # N X T - draw uniform numbers in (0,1)
+    zt       = zeros(Int64, N, T)     # N X T - index on zgrid
+    zt[:,1] .= floor(Int64, z0_idx)  # Set z_0 with valid index
+    CDF      = cumsum(P_z, dims = 2)  # CDF for transition probs. given initial state
 
     @views @inbounds for t = 2:T
         @inbounds for i = 1:N
